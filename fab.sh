c787f653177cd37a871ea4cc2118c9c5f8a1c57b
crides <zhuhaoqing@live.cn>
2023-01-31 19:00:11 -0600

    fabed

--- fab.sh
+++ fab.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 top=10
 vmid=30
 bottom=120
 
-left=55
-hmid1=90
-hmid2=129.5
-right=165
+left=50
+hmid1=88
+hmid2=130
+right=167
 
 kikit_rect() {
     echo "rectangle; tlx: ${1}mm; tly: ${2}mm; brx: ${3}mm; bry: ${4}mm"
@@ -21,3 +23,10 @@ kikit separate -s "$(kikit_rect $hmid1 $top $hmid2 $vmid)" musk.kicad_pcb top.ki
 kibot -e musk.kicad_sch -b top.kicad_pcb jlc-gerbers jlc-drill jlc
 kikit separate -s "$(kikit_rect $hmid2 $top $right $vmid)" musk.kicad_pcb right.kicad_pcb
 kibot -e musk.kicad_sch -b right.kicad_pcb jlc-gerbers jlc-drill jlc
+rm {main,left,top,right}.kicad_*
+
+python - <<EOF
+from case import *
+cq.Assembly().add(gen_bottom(), color=cq.Color("darkseagreen1")).save("bottom.step")
+cq.Assembly().add(gen_top(), color=cq.Color("darkseagreen1")).save("top.step")
+EOF
